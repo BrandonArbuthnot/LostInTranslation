78291050cb504c5f0a025529420a200e668e6c8b
BrandoCommando37 <brandonarbuthnot7@gmail.com>
2019-08-22 13:27:11 -0400

    Added comments for translation, main execution

--- Main.py
+++ Main.py
@@ -12,7 +12,7 @@ auth = tweepy.OAuthHandler(config.CONSUMER_KEY, config.CONSUMER_SECRET)
 auth.set_access_token(config.ACCESS_TOKEN, config.ACCESS_SECRET)
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True, compression=True)
 
-## Function Definitions
+########################################## Function Definitions ####################################################
 
 # Splash Screen
 def splash():
@@ -26,6 +26,7 @@ def splash():
 def start():
     # Difficulty variable
     k = 3
+    j = 20
 
     # Retrieve User input twitter handle
     id_list = prompt_pool()
@@ -38,10 +39,14 @@ def start():
 
     # Pick the tweeter
     answer = random.choice(sampling)
-    tweet_set = (api.user_timeline(answer, count = 20, tweet_mode = 'extended', include_rts = False))
+    tweet_set = (api.user_timeline(answer, count = j, tweet_mode = 'extended', include_rts = False))
 
     # This is the text version of the tweet to be translated
     tweet_text = str(random.choice(tweet_set).full_text.encode('ascii', 'ignore'))
+
+    # print("How many times would you like the tweet to be lost in translation? (Enter a number 1-10):")
+    # z = input()
+    # translate_func(tweet_text, z)
 
     print('************** [The Tweet] ***************\n\"' + tweet_text + '\"\n******************************************')
     print('Whose tweet was this? (Enter a number 1-' + str(k) + ')')
@@ -62,16 +67,18 @@ def start():
     # Reset
     reset_prompt()
 
+# prompt for pool of followed accounts (aka 'friends')
 def prompt_pool():
     print('Please enter a Twitter Handle (ex: user_handle1) to play:')
     user = raw_input()
     if (user == ''): user = 'cse5914'
     return api.friends_ids(user)
 
+# Play again? prompt
 def reset_prompt():
     print("Would you care to play again? Y/n")
     user_in = raw_input()
     if (user_in.lower() == 'y'): start()
 
-# Main execution
+#################################################### Main execution ########################################################
 splash()
