bb98f0863c0e01a304806c0abd75b37b5ecb6e6c
BrandoCommando37 <brandonarbuthnot7@gmail.com>
2019-08-22 12:52:52 -0400

    Initial commit, removing secrets from main LiT file

---
+++ Main.py
@@ -0,0 +1,77 @@
+# Brandon Arbuthnot
+# CSE 5914
+
+# Import libraries
+import tweepy
+import random
+import config
+
+# Setup tweepy to authenticate with Twitter credentials:
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True, compression=True)
+
+## Function Definitions
+
+# Splash Screen
+def splash():
+    print('*****************************************')
+    print('**  [Welcome to: Lost In Translation]  **')
+    print('*****************************************')
+    print('LiT, the social media translation game by Brandon Arbuthnot and Ben Memberg\n')
+    start()
+
+# Primary game function
+def start():
+    # Difficulty variable
+    k = 3
+
+    # Retrieve User input twitter handle
+    id_list = prompt_pool()
+
+    # Narrow choices from pool
+    sampling = random.sample(id_list, k)
+
+    # Translate Twitter id to name
+    user_list = [api.get_user(id).name for id in sampling]
+
+    # Pick the tweeter
+    answer = random.choice(sampling)
+    tweet_set = (api.user_timeline(answer, count = 20, tweet_mode = 'extended', include_rts = False))
+
+    # This is the text version of the tweet to be translated
+    tweet_text = str(random.choice(tweet_set).full_text.encode('ascii', 'ignore'))
+
+    print('************** [The Tweet] ***************\n\"' + tweet_text + '\"\n******************************************')
+    print('Whose tweet was this? (Enter a number 1-' + str(k) + ')')
+
+    # Display tweet candidates
+    for user in user_list:
+        print((str(user_list.index(user) + 1)) + '. ' + user)
+
+    # Player answer
+    player_guess = input()
+
+    # Answer checking
+    if (player_guess-1 == sampling.index(answer)):
+        print("Nicely done! This was " + api.get_user(answer).name + "'s tweet.")
+    else:
+        print("Wrong! This was actually " + api.get_user(answer).name + "'s tweet.")
+
+    # Reset
+    reset_prompt()
+
+def prompt_pool():
+    print('Please enter a Twitter Handle (ex: user_handle1) to play:')
+    user = raw_input()
+    if (user == ''): user = 'cse5914'
+    return api.friends_ids(user)
+
+def reset_prompt():
+    print("Would you care to play again? Y/n")
+    user_in = raw_input()
+    if (user_in.lower() == 'y'): start()
+
+# Main execution
+splash()
